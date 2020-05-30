00e5e9185f3484c17a153dac1854a44c2067523d
alireza <alireza.frsd24@gmail.com>
2020-05-30 14:49:34 +0430

    Updates

---
+++ precache-manifest.50ba1da32c51227494101aea6357dc16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d8f0cfd6dd16bcbcfc02c2ad2b6b99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d34c8a8e1cc0a77e837a",
+    "url": "/static/css/main.21cb3eb1.chunk.css"
+  },
+  {
+    "revision": "d0c732eb512b042044bb",
+    "url": "/static/js/2.28bc1cc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28bc1cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34c8a8e1cc0a77e837a",
+    "url": "/static/js/main.7cf02e82.chunk.js"
+  },
+  {
+    "revision": "bdf80e8a23f3d9dde8f8",
+    "url": "/static/js/runtime-main.5ab5651e.js"
+  }
+]);
